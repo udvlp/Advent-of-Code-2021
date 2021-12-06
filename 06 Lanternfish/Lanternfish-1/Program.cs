d9846e7c9a6e689eb5d3d6edeac7beeb5506a379
udvlp <ulf@dvlp.de>
2021-12-06 15:29:22 +0100

    day 6

---
+++ 06 Lanternfish/Lanternfish-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int days = 80;
+            const int maxage = 8;
+            var sr = new StreamReader(@"..\..\input.txt");
+            int[] age = new int[maxage + 1];
+
+            string l = sr.ReadLine();
+            var p = l.Split(',');
+            foreach (string s in p)
+            {
+                int i = int.Parse(s);
+                age[i]++;
+            }
+
+            for (int i = 0; i < days; i++)
+            {
+                int a0 = age[0];
+                for (int j = 1; j <= maxage; j++)
+                {
+                    age[j - 1] = age[j];
+                    age[j] = 0;
+                }
+                age[8] += a0;
+                age[6] += a0;
+            }
+
+            int e = 0;
+            for (int j = 0; j <= maxage; j++) e += age[j];
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ 06 Lanternfish/Lanternfish-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int days = 256;
+            const int maxage = 8;
+            var sr = new StreamReader(@"..\..\input.txt");
+            long[] age = new long[maxage + 1];
+
+            string l = sr.ReadLine();
+            var p = l.Split(',');
+            foreach (string s in p)
+            {
+                int i = int.Parse(s);
+                age[i]++;
+            }
+
+            for (int i = 0; i < days; i++)
+            {
+                long a0 = age[0];
+                for (int j = 1; j <= maxage; j++)
+                {
+                    age[j - 1] = age[j];
+                    age[j] = 0;
+                }
+                age[8] += a0;
+                age[6] += a0;
+            }
+
+            long e = 0;
+            for (int j = 0; j <= maxage; j++) e += age[j];
+            Console.WriteLine(e);
+        }
+    }
+}
