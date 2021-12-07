527521de85e2fadd45ded33c3ee701c770b70307
udvlp <ulf@dvlp.de>
2021-12-07 12:21:22 +0100

    day 7

---
+++ 07 The Treachery of Whales/TheTreacheryOfWhales-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"..\..\input.txt");
+            string l = sr.ReadLine();
+            var p = l.Split(',');
+            int[] crabs = new int[p.Length];
+            int maxpos = int.MinValue;
+            int minpos = int.MaxValue;
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                crabs[i] = int.Parse(p[i]);
+                if (crabs[i] > maxpos) maxpos = crabs[i];
+                if (crabs[i] < minpos) minpos = crabs[i];
+            }
+
+            int minfuel = int.MaxValue;
+            for (int pos = minpos; pos <= maxpos; pos++)
+            {
+                int fuel = 0;
+                for (int i = 0; i < crabs.Length; i++)
+                {
+                    fuel += Math.Abs(crabs[i] - pos);
+                }
+                if (fuel < minfuel)
+                {
+                    minfuel = fuel;
+                }
+            }
+            Console.WriteLine(minfuel);
+        }
+    }
+}
