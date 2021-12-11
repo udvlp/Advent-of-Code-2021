c95e6c9513b27db91660bfb93a8289dac1e7b4b9
udvlp <ulf@dvlp.de>
2021-12-11 22:32:32 +0100

    day 11

---
+++ 11 Dumbo Octopus/DumboOctopus-2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"..\..\input.txt");
+            int width = lines[0].Length;
+            int height = lines.Length;
+            int[,] field = new int[width, height];
+
+            Console.WriteLine($"{width} x {height}");
+
+            for (int y = 0; y < height; y++)
+            {
+                Trace.Assert(lines[y].Length == width);
+                for (int x = 0; x < width; x++)
+                {
+                    field[x, y] = lines[y][x] - '0';
+                }
+            }
+
+            int i = 0;
+            while (true)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        field[x, y]++;
+                    }
+                }
+
+                bool b = true;
+                while (b)
+                {
+                    b = false;
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            if (field[x, y] > 9)
+                            {
+                                field[x, y] = 0;
+                                for (int dy = -1; dy <= 1; dy++)
+                                {
+                                    for (int dx = -1; dx <= 1; dx++)
+                                    {
+                                        if (dx == 0 && dy == 0 || x + dx < 0 || x + dx >= width || y + dy < 0 || y + dy >= height) continue;
+                                        if (field[x + dx, y + dy] > 0 && field[x + dx, y + dy] <= 9)
+                                        {
+                                            field[x + dx, y + dy]++;
+                                            b = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                b = true;
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        if (field[x, y] != 0)
+                        {
+                            b = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (b)
+                {
+                    Console.WriteLine($"Result: {i + 1}");
+                    break;
+                }
+                i++;
+            }
+
+        }
+    }
+}
