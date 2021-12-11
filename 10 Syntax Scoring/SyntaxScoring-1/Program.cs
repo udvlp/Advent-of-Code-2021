296d38194883c91238c5cec5cb1e8e321d74e9f3
udvlp <ulf@dvlp.de>
2021-12-11 14:57:23 +0100

    day 10 (still trying to catch up)

---
+++ 10 Syntax Scoring/SyntaxScoring-1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 3, 57, 1197, 25137 };
+            string brackets = "([{<)]}>";
+
+            var sr = new StreamReader(@"..\..\input.txt");
+            int totalscore = 0;
+            while (!sr.EndOfStream)
+            {
+                var l = sr.ReadLine();
+                var stack = new Stack<char>();
+
+                foreach (char c in l)
+                {
+                    int i = brackets.IndexOf(c);
+                    if (i < 4)
+                    {
+                        stack.Push(c);
+                    }
+                    else
+                    {
+                        var d = brackets[brackets.IndexOf(stack.Pop()) + 4];
+                        if (c != d)
+                        {
+                            totalscore += scores[i - 4];
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Result: " + totalscore);
+        }
+    }
+}
---
+++ 10 Syntax Scoring/SyntaxScoring-2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string brackets = "([{<)]}>";
+            List<long> scores = new List<long>();
+            var sr = new StreamReader(@"..\..\input.txt");
+            while (!sr.EndOfStream)
+            {
+                var l = sr.ReadLine();
+                var stack = new Stack<char>();
+                bool ok = true;
+
+                foreach (char c in l)
+                {
+                    int i = brackets.IndexOf(c);
+                    if (i < 4)
+                    {
+                        stack.Push(c);
+                    }
+                    else
+                    {
+                        var d = brackets[brackets.IndexOf(stack.Pop()) + 4];
+                        if (c != d)
+                        {
+                            ok = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (ok)
+                {
+                    long score = 0;
+                    while (stack.Count > 0)
+                    {
+                        score = 5 * score + brackets.IndexOf(stack.Pop()) + 1;
+                    }
+                    scores.Add(score);
+                }
+            }
+
+            scores.Sort();
+            Console.WriteLine(scores[scores.Count / 2]);
+        }
+    }
+}
